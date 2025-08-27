5f40bda8b5992cef996e870c1dfab2a1280d8733
wuqingnan-star <qingnan@istarmax.com>
2025-08-27 10:20:55 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wuqingnan-star.github.io/', // 添加这行，使用你的仓库名
   server: {
     port: 3001,
     open: true
